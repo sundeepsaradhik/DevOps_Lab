76b964a0c5b4aaa34cb40d062895ef85bb3ccfc4
sundeepsaradhik <ksundeepsaradhi@gmail.com>
2024-06-07 18:50:03 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@ public class Hello
   public static void main(String args[])
   {
     System.out.println("Welcome to DevOps Lab");
-    System.out.println("07.06.2024, 12:15 PM");
+    System.out.println("07.06.2024, 1849 PM");
   }
 }
